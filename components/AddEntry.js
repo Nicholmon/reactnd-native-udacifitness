90173895c934bfb8a272d1556b4031835bf8bd1e
Nicholas <nicholas@ulmon.com>
2018-08-22 12:56:04 +0200

    Created components folder and AddEntry component with dummy implememtation

---
+++ components/AddEntry.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
+
+export default class AddEntry extends Component {
+  render() {
+    return (
+      <View>
+        {getMetricMetaInfo('bike').getIcon()}
+      </View>
+    )
+  }
+}
