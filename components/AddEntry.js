f531a73a326b01989765b91ac60b78b3af9573a6
Nicholas <nicholas@ulmon.com>
2018-08-22 13:06:07 +0200

    Implemented functions to increment, decrement and slide values and update state component accordingly

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,42 @@ import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
 
 export default class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0
+  }
+
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric)
+     this.setState((prevState) => {
+      const count = prevState[metric] + step
+       return {
+        ...prevState,
+        [metric]: count > max ? max : count,
+      }
+    })
+  }
+
+  decrement = (metric) => {
+    this.setState((prevState) => {
+      const count = prevState[metric] - getMetricMetaInfo(metric).step
+       return {
+        ...prevState,
+        [metric]: count < 0 ? 0 : count,
+      }
+    })
+  }
+
+  slide = (metric, value) => {
+    this.setState((prevState) => ({
+      ...prevState,
+      [metric]: value
+    }))
+  }
+
   render() {
     return (
       <View>
